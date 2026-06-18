a7d892c5db3cb7a68d67d9a0ecf8cd992c45d591
bs24b007-bit <bs24b007@smail.iitm.ac.in>
2026-06-18 17:43:04 +0530

    Update pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,15 +1,16 @@
-# pdf_utils.py
-
 import fitz
 
 def extract_text(pdf_file):
 
-    doc = fitz.open(stream=pdf_file.read(),
-                    filetype="pdf")
+    doc = fitz.open(
+        stream=pdf_file.read(),
+        filetype="pdf"
+    )
 
     text = ""
 
     for page in doc:
+
         text += page.get_text()
 
     return text
